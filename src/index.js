6facd79f19c45ec90f570f20177a10f2383f98d0
Kevin Kwok <antimatter15@gmail.com>
2016-12-24 00:41:46 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,294 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import './index.css';
+
+
+
+
+function reduce(prev, delta){
+    if(!prev) return { version: 0, data: '' };
+
+    return {
+        version: prev.version + 1,
+        data: delta.text
+    }
+}
+
+
+function getState(store, id){
+    var commit = store[id];
+    if(!commit) return null;
+    return reduce(getState(store, commit.parent), commit.delta);
+}
+
+function getPath(store, id){
+    var commit = store[id];
+    if(!commit) return [];
+    return getPath(store, commit.parent).concat([id])
+}
+
+
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/keymap/sublime'
+
+import "codemirror/addon/edit/closebrackets"
+import 'codemirror/addon/edit/matchbrackets'
+import "codemirror/addon/comment/comment"
+
+class CodeEditor extends React.Component {
+    componentDidMount(){
+        var el = ReactDOM.findDOMNode(this);
+        var cm = CodeMirror(el, {
+            value: this.props.value,
+            mode: 'javascript',
+
+            matchBrackets: true,
+            lineNumbers: true,
+            keyMap: 'sublime',
+            autoCloseBrackets: true,
+            lineWrapping: true,
+
+            
+            tabSize: 4,
+            indentUnit: 4,
+            indentWithTabs: false,
+        })
+        this.cm = cm;
+        cm.on('change', (cm, ch) => {
+            if(ch.origin != 'setValue' && cm.getValue() != this.props.value){
+                this.props.onChange(cm.getValue())
+            }
+            // console.log(cm.getValue(), ch)
+            // this.props.onChange(cm.getValue())
+        })
+    }
+    componentDidUpdate(){
+        if(!this.cm.curOp){
+            this.cm.setValue(this.props.value)
+        }
+    }
+    render(){ return <div className="editor" /> }
+}
+
+
+function Interface(props){
+    var state = props.state;
+    if(!state){
+        return <div>(no state)</div>
+    }
+    return <div>
+        <div>
+            <CodeEditor value={state.data} onChange={text => 
+                props.commit({ text: text })} />
+        </div>
+    </div>
+}
+
+
+
+function DAG(props){
+    var store = props.store,
+        view = props.view,
+        messages = props.messages;
+
+    const v_spacing = 20;
+    const h_spacing = 20;
+
+    const v_height = 10;
+
+    function children(node){
+        return Object.keys(store)
+            .filter(k => store[k].parent == node);
+    }
+
+
+    var path = getPath(store, view.anchor);
+
+    var elements = [],
+        lines = [];
+
+    function recursive(node, x, y){
+        var ch = children(node);
+        var trail = [node]
+        while(ch.length == 1 
+            && node 
+            // && view.anchor != node
+            && !props.views.some(k => k.anchor == node)
+            // && view.pointer != node 
+            // && store[ch[0]].date - store[trail[trail.length - 1]].date < 1000
+            ){
+            node = ch[0]
+            trail.push(node)
+            ch = children(node)
+        }
+
+        if(!node){
+
+        }else{
+            // console.log(trail.includes(view.pointer))
+            var rect_width = 10 * Math.sqrt(trail.length + 1)
+            elements.push(<rect 
+                x={x} y={y - v_height/2} 
+                width={rect_width} height={v_height} className={trail.includes(view.pointer) ? 'active' : (
+                    path.includes(node) ? 'mainline' : 'inactive') }
+                onClick={e => props.setPointer(node)}/>);
+
+            if(trail.includes(view.pointer) && trail.length > 1){
+                elements.push(<circle cx={x + rect_width * (trail.indexOf(view.pointer) / (trail.length - 1) )} cy={y} r={3} className="active" />)
+            }
+
+            x += rect_width;
+        }
+
+        var y1 = y;
+        for(var i = 0; i < ch.length; i++){
+            var child = ch[i]
+            if(i > 0) y1 += v_spacing;
+
+            if(node) lines.push(<line 
+                x1={x} y1={y} 
+                x2={x + h_spacing} y2={y1}
+                className={path.includes(child) ? 'mainline' : 'inactive'} />)
+            y1 = recursive(child, x + h_spacing, y1);
+            
+        }
+        return y1;
+    }
+
+    var height = 10 + recursive(null, -h_spacing, 10);
+    return <svg className="dag" height={height}>{lines}{elements}</svg>
+}
+
+
+
+
+function TimeSlice2(props){
+    var state = getState(props.store, props.view.pointer);
+
+    var commit = (delta) => {
+        var id = 'C' + Date.now();
+        props.appendStore(id, { 
+            parent: props.view.pointer, 
+            delta: delta,
+            date: Date.now()
+        })
+        updatePointer(id)
+    }
+
+    var path = getPath(props.store, props.view.anchor);
+
+    var updatePointer = (id) => {
+        if(path.includes(id)){
+            props.updateView({ pointer: id })    
+        }else{
+            // TODO: find a suitable end-of-line for anchor
+            props.updateView({ pointer: id, anchor: id })
+        }
+    }
+
+    var pathIndex = path.indexOf(props.view.pointer);
+
+    return <div className="artboard">
+        <input type="text" className="title" value={props.view.title} 
+            onChange={e=>props.updateView({ title: e.target.value }) }/>
+
+        <Interface state={state} commit={commit} />
+
+        <input type="range" className="linear" min={0} max={path.length - 1} 
+            disabled={path.length < 2}
+            onChange={e => updatePointer(path[e.target.value])}
+            value={path.indexOf(props.view.pointer)} />
+        <button 
+            disabled={pathIndex <= 0}
+            onClick={e => updatePointer(path[pathIndex - 1])}>Undo</button>
+        <button 
+            disabled={pathIndex >= path.length - 1}
+            onClick={e => updatePointer(path[pathIndex + 1])}>Redo</button>
+        <button onClick={e => props.fork()}>Fork</button>
+        <button onClick={e => props.close()}>&times;</button>
+        <DAG 
+            store={props.store} 
+            view={props.view} 
+            views={props.views}
+            setPointer={updatePointer} />
+        
+    </div>
+}
+
+const DEFAULT_STATE = {
+    store: {
+        '0': {
+            parent: null,
+            delta: { },
+            date: 0
+        }
+    },
+    messages: {},
+    views: [{
+        title: 'Default',
+        pointer: '0',
+        anchor: '0'
+    }]
+};
+
+
+class StateKeeper extends React.Component {
+    constructor(){
+        super()
+        try {
+            this.state = JSON.parse(localStorage.state)
+        }catch(e){
+            this.state = JSON.parse(JSON.stringify(DEFAULT_STATE))
+        }
+        
+    }
+    componentDidUpdate(){
+        localStorage.state = JSON.stringify(this.state)
+    }
+    render(){
+        var views = this.state.views;
+        return <div>
+            <div className="container">{
+                views.map((view, index) => 
+                    <TimeSlice2 
+                        store={this.state.store}
+                        appendStore={(id, value) => this.setState({ store: 
+                            Object.assign({}, this.state.store, { [id]: value }) })}
+                        view={view}
+                        views={views}
+                        messages={this.state.messages}
+                        updateView={data => this.setState({ views: 
+                            views.slice(0, index)
+                            .concat([Object.assign({}, view, data)], 
+                            views.slice(index + 1)) })}
+                        fork={e => this.setState({ views: views.concat([ Object.assign({}, view, { id: 'V' + Date.now() }) ]) })}
+                        close={e => this.setState({ views: views.slice(0, index).concat(views.slice(index+1)) })}
+                    />)
+            }</div>
+            <div className="controls">
+                <button onClick={e => this.setState(DEFAULT_STATE)}>Reset</button>
+            </div>
+        </div>
+    }
+}
+
+
+
+function App(props){
+    return <div className="app">
+        <div className="header">
+            <h1>Derp <span>kinda like version control or something</span></h1>
+        </div>
+        <StateKeeper />
+    </div>
+}
+
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);

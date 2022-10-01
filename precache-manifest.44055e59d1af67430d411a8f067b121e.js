ae7221a87e5ebc11e3ac518bdacb3bdfb39cfd20
Kevin Kwok <antimatter15@gmail.com>
2022-10-01 02:23:29 +0200

    Updates

---
+++ precache-manifest.44055e59d1af67430d411a8f067b121e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a38a780fc032e490df3",
+    "url": "./static/css/main.943f328f.chunk.css"
+  },
+  {
+    "revision": "5a38a780fc032e490df3",
+    "url": "./static/js/main.4be95278.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "43f4373a48977e7d529c",
+    "url": "./static/css/2.3259fc5d.chunk.css"
+  },
+  {
+    "revision": "43f4373a48977e7d529c",
+    "url": "./static/js/2.c96570a5.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e43f4846ae4d0f71f4459a6aa705ab7f",
+    "url": "./index.html"
+  }
+];
